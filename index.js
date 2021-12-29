d41467128c10095043bdbdfb2b3bc5696fc5e70d
AnilBameta <anilbameta@gmail.com>
2021-12-29 16:11:53 +0530

    Update

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const cors = require('cors');
 mongoose.connect('mongodb+srv://AnilBameta:AnilgotAtlas@cluster0.hw2so.mongodb.net/SignUpData?retryWrites=true&w=majority').then (()=>
     console.log("Connection Success")
 ).catch((err)=>console.log(err));
 mongoose.Promise = global.Promise;
+app.use(cors())
  app.use(express.static('public'));
  app.use(express.json());
  app.use('/api',require('./routes/api'));
