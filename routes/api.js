fdc9f5da74a9a2fbcc43be4b3970e1e69d3f9172
AnilBameta <anilbameta@gmail.com>
2021-12-30 13:46:35 +0530

    LogIn Update

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+router.get('/user',function(req,res,next){
+    User.find({}).then(function(user){
+        res.send(user);
+    }).catch(next);
+});
 router.post('/user',function(req,res,next){
     User.create(req.body).then(function(user){
         res.send(user);
