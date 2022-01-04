65ee05e263f13531bbf9b7596e8ce83468cc6857
AnilBameta <anilbameta@gmail.com>
2022-01-04 11:48:46 +0530

    Movie Array added

--- routes/api.js
+++ routes/api.js
@@ -51,33 +51,52 @@ router.post('/Sign',(req,res,next)=> {
             }
         })
     })
-
+   let name="Anil"
  router.post('/watchlist' , (req,res,next) => {
-            WatchList.find({ UserName : req.body.UserName} )
-            .exec()
-            .then(response => {
-                if(response.length >= 1)
+            WatchList.findOneAndUpdate ({UserName : req.body.UserName}, {$addToSet : { MovieList : req.body.Movie}},(error,data) => {
+                if(error)
                 {
-                    {
-                        $addToSet: { MovieList : req.body.Movie}
-                    }
+                    return error
                 }
                 else {
-                    const watchList =new WatchList( {
-                        UserName : req.body.UserName,
-                        Movie : req.body.Movie,
-                        $addToSet : {
-                            MovieList : req.body.Movie,
-                           }
-                        })
-                    watchList.save()
-                    .then(()=> res.json())
-                    .catch(err=> err)
+                    console.log(data)
+                    res.status(401).json({
+                        message:'Added Succesfully'
+                    })
                 }
-            
-            })
-                }
+            }
             )
+            // WatchList.find({ UserName : req.body.UserName} )
+            // .exec()
+            // .then(response => {
+            //     if(response.length >= 1)
+            //     {
+            //         var myquery = {MovieList: req.body.MovieList}
+            //         var newvalues =  {$addToSet : {
+            //             MovieList : req.body.Movie,
+            //            }};
+            //         response[0].updateOne(myquery, newvalues, function(err, res) {
+            //             if (err) throw err;
+            //             console.log("1 document updated");
+                        
+            //     })
+            // }  
+            //     else {
+            //         const watchList =new WatchList( {
+            //             UserName : req.body.UserName,
+            //             Movie : req.body.Movie,
+            //             $addToSet : {
+            //                 MovieList : req.body.Movie,
+            //                }
+            //             })
+            //         watchList.save()
+            //         .then(()=> res.json())
+            //         .catch(err=> err)
+            //     }
+            
+            // })
+                 }
+             )
              
 
 
