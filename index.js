cd9b8ba8f2cec748fb54675631bdff7adc643a48
AnilBameta <anilbameta@gmail.com>
2022-01-10 22:08:25 +0530

    Update

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose.Promise = global.Promise;
     res.status(401).send({error: err.message});
     res.status(500).send({error: err.message});
  });
- app.listen(process.env.PORT || 8000, () => {
+ app.listen(process.env.PORT || 8080, () => {
     console.log('Ready to go');
  });
  
--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const User = require('../models/user');
 const WatchList = require('../models/watchList');
 const MovieCount = require('../models/movieCount');
-const GenreWise =require('../models/genrewise');
-const GenreWiseCount =require('../models/genrewiseCount');
+const GenreWise = require('../models/genrewise');
+const GenreWiseCount = require('../models/genrewiseCount');
 
 
 router.post('/Sign', (req, res, next) => {
@@ -82,12 +82,12 @@ router.post('/watchlist', (req, res, next) => {
     )
 
 }
-) 
+)
 
 // let max=1
 // let sendItem =''
 // router.get('/watchlist', (req, res, next) => {
-   
+
 //     WatchList.find({}).then(function(response){
 //             res.send(response);
 //             let m =[]
@@ -98,7 +98,7 @@ router.post('/watchlist', (req, res, next) => {
 //                 })
 //                 })
 //                 console.log(m)
-                   
+
 
 
 
@@ -119,193 +119,194 @@ router.post('/watchlist', (req, res, next) => {
 // console.log(`${item} ( ${mf} times ) `) ;
 
 
-                // let uniqueM = []
-                // m.forEach((c) => {
-                //     if (!uniqueM.includes(c)) {
-                //         uniqueM.push(c);
-                //     }
-                // });
-                // console.log(uniqueM)
-                
-                // uniqueM.map(item => {
-                //     function getOccurrence(array, value) {
-                //         var count = 0;
-                //         array.forEach((v) => (v.localeCompare(value)===0 && count++));
-                //         return count;
-                //     }
-                //     let output=getOccurrence(m, item)
-                   
-                //     if(output>=max)
-                //     {
-                //         max=output
-                //         sendItem = item
-                //     }
-                    
-                // })
-        //         console.log(sendItem,max);
+// let uniqueM = []
+// m.forEach((c) => {
+//     if (!uniqueM.includes(c)) {
+//         uniqueM.push(c);
+//     }
+// });
+// console.log(uniqueM)
 
-                
-        //         MovieCount.insertMany({
-            
-        //             "Movie":sendItem,
-        //             "count":max
-               
-        //     }) 
+// uniqueM.map(item => {
+//     function getOccurrence(array, value) {
+//         var count = 0;
+//         array.forEach((v) => (v.localeCompare(value)===0 && count++));
+//         return count;
+//     }
+//     let output=getOccurrence(m, item)
 
-        //     }).catch(err => err);
-        // })
+//     if(output>=max)
+//     {
+//         max=output
+//         sendItem = item
+//     }
 
-        router.get('/watchlist', async (req, res, next) => {
-                    await WatchList.aggregate(
-                    [
-                      { $unwind : "$MovieList" },
-                      { $group : { _id : "$MovieList" , count : { $sum : 1 } } },
-                      { $sort : { count : -1 } },
-                      { $limit : 1 }
-                    ]
-                  )
-                  .then( response =>
-                    MovieCount.insertMany({
-            
-                                    "Movie":response[0]._id,
-                                    "count":response[0].count
-                               
-                            }) 
-                     
-                  )
-                  .catch(err => err)
+// })
+//         console.log(sendItem,max);
+
+
+//         MovieCount.insertMany({
+
+//             "Movie":sendItem,
+//             "count":max
+
+//     }) 
+
+//     }).catch(err => err);
+// })
+
+router.get('/watchlist', async (req, res, next) => {
+    await WatchList.aggregate(
+        [
+            { $unwind: "$MovieList" },
+            { $group: { _id: "$MovieList", count: { $sum: 1 } } },
+            { $sort: { count: -1 } },
+            { $limit: 1 }
+        ]
+    )
+        .then(response =>
+            MovieCount.insertMany({
+
+                "Movie": response[0]._id,
+                "count": response[0].count
+
             })
 
+        )
+        .catch(err => err)
+})
 
-           
-           
-           
 
 
-        router.get('/movieCount', (req, res, next) => {
-            MovieCount.find({"Movie":sendItem})
-            .then(response => res.send(response))
-            .catch(err=>err)
-            
-        })
-       
-       router.post('/genreWise',(req,res,next) => {
-         GenreWise.findOneAndUpdate({"Genre": req.body.Genre}, { $push: { MoviesList: req.body.Movie } }, { upsert: true }, (error, data) => {
-            if (error) {
-                return error
-            }
-            else {
-                
-                res.json({
-                    message: 'Added Succesfully'
-                })
-         }
-        })
 
-        let mov =[] ;
-        let m=0,mf=1,item='';
-        GenreWise.find({"Genre": req.body.Genre})
+
+
+
+router.get('/movieCount', (req, res, next) => {
+    MovieCount.find({ "Movie": sendItem })
+        .then(response => res.send(response))
+        .catch(err => err)
+
+})
+
+router.post('/genreWise', (req, res, next) => {
+    GenreWise.findOneAndUpdate({ "Genre": req.body.Genre }, { $push: { MoviesList: req.body.Movie } }, { upsert: true }, (error, data) => {
+        if (error) {
+            return error
+        }
+        else {
+
+            res.json({
+                message: 'Added Succesfully'
+            })
+        }
+    })
+
+    let mov = [];
+    let m = 0, mf = 1, item = '';
+    GenreWise.find({ "Genre": req.body.Genre })
         .then(response => {
             mov = response[0].MoviesList
             console.log(mov)
 
-            for (let i=0; i<mov.length; i++)
-{
-        for (let j=i; j<mov.length; j++)
-        {
-                if (mov[i] == mov[j])
-                 m++;
-                if (mf<m)
-                {
-                  mf=m; 
-                  item = mov[i];
+            for (let i = 0; i < mov.length; i++) {
+                for (let j = i; j < mov.length; j++) {
+                    if (mov[i] == mov[j])
+                        m++;
+                    if (mf < m) {
+                        mf = m;
+                        item = mov[i];
+                    }
                 }
-        }
-        m=0;
-}
-console.log(`${item} ( ${mf} times ) `) ;
+                m = 0;
+            }
+            console.log(`${item} ( ${mf} times ) `);
 
-GenreWiseCount.findOneAndUpdate({"Genre": req.body.Genre},
-{"Genre": req.body.Genre,
- "Movie":item,
- "count":mf
-},{upsert:true},
-(error, data) => {
-    if (error) {
-        return error
-    }
-    else {
-        
-        res.json({
-            message: 'Added Succesfully'
+            GenreWiseCount.findOneAndUpdate({ "Genre": req.body.Genre },
+                {
+                    "Genre": req.body.Genre,
+                    "Movie": item,
+                    "count": mf
+                }, { upsert: true },
+                (err, data) => {
+                    if (err) {
+                        return err
+                    }
+                    else {
+
+                        res.json({
+                            message: 'Added Succesfully'
+                        })
+                    }
+                }
+            )
+
+
         })
- }}
- )
+        .catch(err => 
+            cosnole.log(err))
+
+})
 
 
-        })
-        .catch(err => err)
 
-       
+// GenreWiseCount.findOneAndUpdate({"Genre": req.body.Genre})
 
 
-        // GenreWiseCount.findOneAndUpdate({"Genre": req.body.Genre})
+// GenreWise.find({"Genre": req.body.Genre})
+// .then(response => {
+//     response[0].aggregate(
+//         [
+//           { $unwind : "$MoviesList" },
+//           { $group : { _id : "$MoviesList" , count : { $sum : 1 } } },
+//           { $sort : { count : -1 } },
+//           { $limit : 1 }
+//         ]
+//       )
+//       .then( result => {
+//           GenreWiseCount.insertMany(
+//             {
+//                 "Genre":req.body.Genre,
+//                 "Movie":result[0]._id,
+//                 "count":result[0].count
+
+//         }
+//           )
+//       }
+
+//       )
+// })
 
 
-        // GenreWise.find({"Genre": req.body.Genre})
-        // .then(response => {
-        //     response[0].aggregate(
-        //         [
-        //           { $unwind : "$MoviesList" },
-        //           { $group : { _id : "$MoviesList" , count : { $sum : 1 } } },
-        //           { $sort : { count : -1 } },
-        //           { $limit : 1 }
-        //         ]
-        //       )
-        //       .then( result => {
-        //           GenreWiseCount.insertMany(
-        //             {
-        //                 "Genre":req.body.Genre,
-        //                 "Movie":result[0]._id,
-        //                 "count":result[0].count
-                   
-        //         }
-        //           )
-        //       }
-
-        //       )
-        // })
 
 
-       })
 
-         
-        // router.post('/movieCount', (req, res, next) => {
+// router.post('/movieCount', (req, res, next) => {
 
-        //     MovieCount.find({ Movie: req.body.Movie })
-        //         .exec()
-        //         .then(response => {
-        //             if (response.length < 1) {
-        //                 const movieCount = new MovieCount({
-        //                     Movie: req.body.Movie,
-        //                     count: req.body.count
-        //                 })
-        //                 movieCount.save()
-        //                     .then(() => res.json())
-        //                     .catch((err) => err)
-        //             }
-        //             else {
-        //               MovieCount.update({Movie: req.body.Movie,Count: req.body.Count},{$set:{Movie:sendItem,Count:max}},(err => console.log(err)))
-        //             }
-        //         })
-        //         .catch(err => err)
-            
-        //     })
-       
-       
-   
-     
-    
+//     MovieCount.find({ Movie: req.body.Movie })
+//         .exec()
+//         .then(response => {
+//             if (response.length < 1) {
+//                 const movieCount = new MovieCount({
+//                     Movie: req.body.Movie,
+//                     count: req.body.count
+//                 })
+//                 movieCount.save()
+//                     .then(() => res.json())
+//                     .catch((err) => err)
+//             }
+//             else {
+//               MovieCount.update({Movie: req.body.Movie,Count: req.body.Count},{$set:{Movie:sendItem,Count:max}},(err => console.log(err)))
+//             }
+//         })
+//         .catch(err => err)
+
+//     })
+
+
+
+
+
 //  router.post('/movieCount', (req, res, next) => {
 // MovieCount.find({Movie: req.body.Movie})
 //     .exec()
