790873721388742fad1dea849c5cef3fa52d26e1
AnilBameta <anilbameta@gmail.com>
2022-01-13 11:09:07 +0530

    Update

--- models/genrewise.js
+++ models/genrewise.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const genreSchema = new Schema( {
-    Username:{
+    UserName:{
         type:[String]
     },
     Genre:{ 
